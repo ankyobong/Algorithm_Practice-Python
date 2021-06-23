f065df610587eed19219014602d906bd209a40e6
ankyobong <ankyobong@gmail.com>
2021-06-23 10:52:56 +0900

    두개의 숫자를 뽑아서 더하는 코드

---
+++ level1_sum_two_number.py
@@ -0,0 +1,12 @@
+from itertools import combinations
+
+
+def solution(numbers):
+    answer = []
+    for i in combinations(numbers,2):
+        answer.append(sum(i))
+
+    return sorted(list(set(answer)))
+
+
+print(solution([2, 1, 3, 4, 1]))
