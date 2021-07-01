caf6e8e8fa4b5414ece54c31afc173669c782f56
ankyobong <ankyobong@gmail.com>
2021-07-01 14:35:34 +0900

    basic 100문제.. 98번까지 왜 100문제라했는데 98번이 끝인가...

--- codeup/python_ basic_100.py
+++ codeup/python_ basic_100.py
@@ -533,3 +533,62 @@ for i in range(1, 20):
     for j in range(1, 20):
         print(d[i][j], end=' ')
     print()
+
+# 96
+d = [0 for i in range(19)]
+for i in range(19):
+    d[i] = (list(map(int, input().split())))
+n = int(input())
+for i in range(n):
+    x, y = input().split()
+    for j in range(19):
+        if d[j][int(y)-1] == 0:
+            d[j][int(y)-1] = 1
+        else:
+            d[j][int(y)-1] = 0
+
+        if d[int(x)-1][j] == 0:
+            d[int(x)-1][j] = 1
+        else:
+            d[int(x)-1][j] = 0
+for i in range(19):
+    for j in range(19):
+        print(d[i][j], end=' ')
+    print()
+
+# 97
+h, w = map(int, input().split())
+a = [[0 for j in range(w)] for i in range(h)]
+n = int(input())
+for i in range(n):
+    l, d, x, y = map(int, input().split())
+    for j in range(l):
+        if d == 0:
+            a[x-1][y-1+j] = 1
+        else:
+            a[x-1+j][y-1] = 1
+for i in range(h):
+    for j in range(w):
+        print(a[i][j], end=' ')
+    print()
+# 98
+a = [[0 for z in range(10)] for y in range(10)]
+for i in range(10):
+    a[i] = list(map(int, input().split()))
+c, d = 1, 1
+for i in range(1,10):
+    for j in range(1,10):
+        if a[i][j] == 0 and c == j:
+            a[i][j] = 9
+            c = j+1
+        elif a[i][j] == 1 and d == i and c == j:
+            c = j-1
+            d = i+1
+        elif a[i][j] == 2 and d == i and c == j:
+            a[i][j] = 9
+            c = None
+            d = None
+for i in range(10):
+    for j in range(10):
+        print(a[i][j], end=' ')
+    print()
