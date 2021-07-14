414bfd9197fcaa42979484755e0a7a8665d6824e
ankyobong <ankyobong@gmail.com>
2021-07-14 11:29:16 +0900

    짝지어 제거하기 한번에 다바꾸려고 하니.... 런타임 초과 하나씩 체크하는게 더빠르고 효율적....

---
+++ level2/level2_twin_remove.py
@@ -0,0 +1,16 @@
+def solution(s):
+    a = [0]
+    s = ",".join(s)
+    s = s.split(',')
+    for i, j in enumerate(s):
+        if a[-1] == j:
+            a.pop(-1)
+        else:
+            a.append(j)
+    if len(a) == 1:
+        return 1
+    else:
+        return 0
+
+print(solution('baabaa'))
+print(solution('cdcd'))
