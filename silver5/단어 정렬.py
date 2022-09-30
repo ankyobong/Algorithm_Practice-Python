95411fbb7e9667fce1ae282aea626c20442f0c37
an kyo bong <akb0930@vivans.net>
2022-09-30 17:29:57 +0900

    단어 정렬

---
+++ silver5/단어 정렬.py
@@ -0,0 +1,16 @@
+# https://www.acmicpc.net/problem/1181
+# a = ['but', 'i', 'wont', 'hesitate', 'no', 'more', 'no', 'more', 'it', 'cannot', 'wait', 'im', 'yours']
+import sys
+a = set()
+for i in range(int(input())):
+    a.add(sys.stdin.readline().rstrip())
+a = list(a)
+a.sort()
+a.sort(key=lambda x: len(x))
+print('\n'.join(a))
+# s_list = sorted(a, key=lambda a: len(a), reverse=False)
+# print('\n'.join(s_list))
+
+# sorted 보다 리스트에 내장함수 적용하는게 더 좋음.
+# 값입력시 함수 사용하면 더빠르게 읽음.
+
