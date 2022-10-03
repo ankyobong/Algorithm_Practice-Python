59b831df52429162d3b40257e41940fe29188268
kyobong An <akb0930@vivans.neet>
2022-10-03 22:20:24 +0900

    다리나누기

---
+++ silver5/다리나누기.py
@@ -0,0 +1,17 @@
+word = list(input())
+result = []
+tmp = list()
+for i in range(1, len(word)-1):
+    for j in range(i + 1, len(word)):
+        a = word[:i]
+        b = word[i:j]
+        c = word[j:]
+        a.reverse()
+        b.reverse()
+        c.reverse()
+        tmp.append(a + b + c)
+
+for a in tmp:
+    result.append(''.join(a))
+
+print(sorted(result)[0])
