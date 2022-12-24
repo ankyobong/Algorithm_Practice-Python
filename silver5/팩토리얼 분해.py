6eb8943c175837581581a6e96be4d62eeb1f76f4
ankyobong <ankyobong@gmail.com>
2022-12-24 23:58:48 +0900

    팩토리얼 분해

---
+++ silver5/팩토리얼 분해.py
@@ -0,0 +1,14 @@
+# https://www.acmicpc.net/problem/2057
+import math
+n = int(input())
+result = 0
+fac_num = [math.factorial(i) for i in range(21)]
+
+if n == 0:
+    print()
+else:
+    for i in range(20, -1,-1):
+        if n >= fac_num[i]:
+            n -= fac_num[i]
+
+    print("YES") if n ==0 else print("NO")
