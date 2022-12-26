5c3248c818f9f3b33850e60a400797b24c92dd4f
ankyobong <ankyobong@gmail.com>
2022-12-27 01:31:14 +0900

    콘도 선정

---
+++ silver5/콘도 선정.py
@@ -0,0 +1,16 @@
+# https://www.acmicpc.net/problem/2246
+
+condos = []
+for i in range(int(input())):
+    l, p = map(int, input().split())
+    condos.append([l, p])
+condos.sort()
+cost = 10001
+result = 0
+for i in condos:
+    temp = i[1]
+    if temp < cost:
+        cost = temp
+        result += 1
+print(result)
+
