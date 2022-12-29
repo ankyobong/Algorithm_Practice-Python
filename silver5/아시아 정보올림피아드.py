e1e3d4db28fb905f45d91903dde54288bfd4e372
ankyobong <ankyobong@gmail.com>
2022-12-30 01:09:27 +0900

    아이사 정보올림피아드 수정

--- silver5/아시아 정보올림피아드.py
+++ silver5/아시아 정보올림피아드.py
@@ -1,5 +1,4 @@
 # https://www.acmicpc.net/problem/2535
-input = __import__('sys').stdin.readline
 n = int(input())
 arr = []
 cnt = 0
@@ -9,13 +8,10 @@ for _ in range(n):
 arr = sorted(arr, key=lambda x: -x[2])
 print(arr[0][0], arr[0][1])
 print(arr[1][0], arr[1][1])
-if arr[0][0] == arr[1][0]: cnt = 1
-
-for i in range(2, n):
-    if cnt == 0:
-        print(arr[i][0], arr[i][1])
-        break
-    else:
+if arr[0][0] != arr[1][0]:
+    print(arr[2][0], arr[2][1])
+else:
+    for i in range(2, n):
         if arr[1][0] != arr[i][0]:
             print(arr[i][0], arr[i][1])
             break
