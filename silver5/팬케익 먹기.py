6560cbdc81dab70785f3070e4ba45c8edf19f302
ankyobong <ankyobong@gmail.com>
2022-12-31 23:04:49 +0900

    팬케익 먹기

---
+++ silver5/팬케익 먹기.py
@@ -0,0 +1,13 @@
+# https://www.acmicpc.net/problem/2545
+t = int(input())
+result = []
+for _ in range(t):
+    input()
+    a, b, c, d = map(int, input().split())
+    a, b, c = sorted((a, b, c))
+    s = a + b + c - d
+    tmp = min(s // 3, a)
+    a1 = tmp
+    s -= tmp
+    tmp = min(s // 2, b)
+    print(a1*tmp*(s-tmp))
