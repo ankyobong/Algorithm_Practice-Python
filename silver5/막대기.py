4a9bde550108e1d2bcca65f75259b4c971aacaa7
ankyobong <ankyobong@gmail.com>
2023-01-03 23:55:55 +0900

    막대기

--- silver5/막대기.py
+++ silver5/막대기.py
@@ -1,14 +1,15 @@
 # https://www.acmicpc.net/problem/1094
 
-# x = int(input())
-# y = [64]
-# while int(sum(y)) != x:
-#     y[-1] /= 2
-#     if sum(y) > x:
-#         continue
-#     elif int(sum(y)) == x:
-#         break
-#     y.append(y[-1])
-#
-# print(len(y))
-print(bin(int(input())).count('1'))
+x = int(input())
+y = [64]
+while int(sum(y)) != x:
+    y[-1] /= 2
+    if sum(y) > x:
+        continue
+    elif int(sum(y)) == x:
+        break
+    y.append(y[-1])
+
+print(len(y))
+
+# print(bin(int(input())).count('1'))
