314a0698f4a5f10614634d0501365108bd9681ed
ankyobong <ankyobong@gmail.com>
2023-01-04 21:35:16 +0900

    행렬 곱셈 수정

--- silver5/행렬 곱셈.py
+++ silver5/행렬 곱셈.py
@@ -1,17 +1,17 @@
 # https://www.acmicpc.net/problem/2740
 n, m = map(int, input().split())
-a = [[map(int, input().split())]for _ in range(n)]
+a = [[*map(int, input().split())]for _ in range(n)]
 m, k = map(int, input().split())
-b = [[map(int, input().split())]for _ in range(m)]
+b = [[*map(int, input().split())]for _ in range(m)]
 
 c = [[0 for _ in range(k)] for _ in range(n)]
 
-for n in range(n):
-    for k in range(k):
-        for m in range(m):
-            c[n][k] += a[n][m] * b[m][k]
+for sn in range(n):
+    for sk in range(k):
+        for sm in range(m):
+            c[sn][sk] += a[sn][sm] * b[sm][sk]
 
 for i in c:
     for j in i:
-        print(j, end = ' ')
+        print(j, end=' ')
     print()
