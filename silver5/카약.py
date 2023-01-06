9df19855f21c318d80a1fb79c50d6c5092870c35
ankyobong <ankyobong@gmail.com>
2023-01-07 00:39:06 +0900

    카

--- silver5/카약.py
+++ silver5/카약.py
@@ -1,17 +1,19 @@
 # https://www.acmicpc.net/problem/2890
 import re
-le = [0 for _ in range(10)]
+le = [-1 for _ in range(9)]
 r, c = map(int, input().split())
 
-for i in range(r):
-    l = input()
-    idx = re.sub(r'[^1-9]', '', l)
-    if idx:
-        le[int(idx[0])] = c - l.rfind(idx[0]) - 1
+for _ in range(r):
+    idx = re.split('[1-9]{2}', input())
+    if idx[0].count('.') != (c - 2):
+        le[int(idx[1][0])-1] = ((c-5) - idx[0].count('.'))
 
-rank = list(set(sorted(le)))
-result = ''.join([str(i) for i in le])
-for n, i in enumerate(rank):
-    result = result.replace(str(i), str(n))
-for i in result[1:]:
+rank = 1
+d = {}
+for num in sorted(le):
+    if num not in d:
+        d[num] = rank
+        rank += 1
+result = [d[i] for i in le]
+for i in result:
     print(i)
