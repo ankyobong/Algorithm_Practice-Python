cc450184441184aa3f3a409dd85724a1b03027fc
ankyobong <ankyobong@gmail.com>
2023-01-08 20:13:24 +0900

    채식주의자

---
+++ silver5/채식주의자.py
@@ -0,0 +1,26 @@
+# https://www.acmicpc.net/problem/2936
+x, y = map(int, input().split())
+s = 31250  # 한변의 넓이
+l = 125  # 빗변의 제곱
+r_x, r_y = 0, 0
+if x == 0:
+    if y == 0:
+        r_x = r_y = l
+    elif y < l:
+        r_x = s/(250 - y)
+        r_y = 250 - r_x
+    else:
+        r_x = s/y
+        r_y = 0
+elif y == 0:
+    if x < l:
+        r_y = s/(250-x)
+        r_x = 250 - r_y
+    else:
+        r_y = s/x
+else:
+    if x < l:
+        r_x = 250-s/y
+    else:
+        r_y = 250-s/x
+print("%.2f %.2f" % (r_x, r_y))
