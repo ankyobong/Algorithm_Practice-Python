8ab360fc813a70d3ae652daa40ffbbfe5a26ca0a
ankyobong <ankyobong@gmail.com>
2023-01-19 22:57:53 +0900

    표

---
+++ silver5/표.py
@@ -0,0 +1,13 @@
+# https://www.acmicpc.net/problem/3724
+for _ in range(int(input())):
+    m, n = map(int, input().split())
+    chart = []
+    for i in range(n):
+        chart.append([*map(int, input().split())])
+    result = {}
+    for i in range(m):
+        t = 1
+        for j in range(n):
+            t *= chart[j][i]
+        result[t] = i+1
+    print(result[max(result)])
