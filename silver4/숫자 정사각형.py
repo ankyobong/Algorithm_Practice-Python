e831b22bd04ef8b20261b9d01353f8f52c37c968
ankyobong <ankyobong@gmail.com>
2023-01-26 22:56:19 +0900

    숫자 정사각형

---
+++ silver4/숫자 정사각형.py
@@ -0,0 +1,18 @@
+# https://www.acmicpc.net/problem/1051
+n, m = map(int, input().split())
+mx = min(n, m)-1
+square = []
+for _ in range(n):
+    square.append(input())
+result = 1
+while mx != 0:
+    for i in range(n-mx):
+        for j in range(m-mx):
+            s = square[i][j]
+            if s == square[i+mx][j] and s == square[i][j+mx] and s == square[i+mx][j+mx]:
+                result = (mx+1)**2
+                print(result)
+                exit(0)
+    mx -= 1
+
+print(result)
