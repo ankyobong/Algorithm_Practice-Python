27f55f331ab604c8fef558d4d28711be71f2e102
ankyobong <ankyobong@gmail.com>
2023-01-28 23:21:19 +0900

    토너먼트

---
+++ silver4/토너먼트.py
@@ -0,0 +1,11 @@
+# https://www.acmicpc.net/problem/1057
+n, a, b = map(int, input().split())
+round_v = 0
+while True:
+    if a == b:
+        break
+    a = (a+1)//2
+    b = (b+1)//2
+    round_v = round_v+1
+
+print(round_v)
