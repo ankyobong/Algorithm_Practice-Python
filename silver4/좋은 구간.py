4350267be394d4b83921c2839e86b0bbd581a3e8
ankyobong <ankyobong@gmail.com>
2023-01-30 01:44:30 +0900

    좋은 구간

--- silver4/좋은 구간.py
+++ silver4/좋은 구간.py
@@ -4,10 +4,16 @@ s = [*map(int, input().split())]
 n = int(input())
 s.sort()
 result = 0
+if n in s:
+    print(0)
+    exit(0)
 for i in range(s_len):
     if s[i] < n < s[i+1]:
-        a = s[i]
+        a = s[i]+1
         b = s[i+1]
         break
-for i in range(a+1, b):
-    
+for i in range(a, b):
+    for j in range(i+1, b):
+        if j >= n >= i:
+            result += 1
+print(result)
