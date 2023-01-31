bd2547e11a8b9269ae259dc23a188624b3b4bdce
ankyobong <ankyobong@gmail.com>
2023-02-01 00:45:36 +0900

    문자열

---
+++ silver4/문자열.py
@@ -0,0 +1,12 @@
+# https://www.acmicpc.net/problem/1120
+a, b = input().split()
+
+result = 50
+for n in range(len(b)-len(a)+1):
+    s = 0
+    for i in range(len(a)):
+        if a[i] != b[i+n]:
+            s += 1
+    if result > s:
+        result = s
+print(result)
