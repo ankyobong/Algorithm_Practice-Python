7c1e2e3ab0ab9935ecb905ec5dd15134138342d1
ankyobong <ankyobong@gmail.com>
2023-02-15 23:21:59 +0900

    올바른 배열

--- silver4/올바른 배열.py
+++ silver4/올바른 배열.py
@@ -3,3 +3,13 @@ n = int(input())
 l = []
 for i in range(n):
     l.append(int(input()))
+l.sort()
+result = []
+for i in l:
+    t = 0
+    for j in range(i, i+5):
+        if j not in l:
+            t += 1
+    result.append(t)
+
+print(min(result))
