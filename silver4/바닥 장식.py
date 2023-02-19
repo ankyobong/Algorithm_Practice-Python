53c10ee1d8014c36d11b3cda1421ef74a8e1d49b
ankyobong <ankyobong@gmail.com>
2023-02-19 14:54:12 +0900

    바닥 장식

--- silver4/바닥 장식.py
+++ silver4/바닥 장식.py
@@ -1,5 +1,27 @@
 # https://www.acmicpc.net/problem/1388
+def dfs(x, y):
+    if floor[x][y] == '-':
+        floor[x][y] = 1
+        for _y in (1, -1):
+            ty = y + _y
+            if (0 < ty < m) and floor[x][ty] == '-':
+                dfs(x, ty)
+    elif floor[x][y] == '|':
+        floor[x][y] = 1
+        for _x in (1, -1):
+            tx = x + _x
+            if (0 < tx < n) and floor[tx][y] == '|':
+                dfs(tx, y)
+
+
 n, m = map(int, input().split())
 floor = list()
 for _ in range(n):
-    floor.append(input())
+    floor.append(list(input()))
+result = 0
+for i in range(n):
+    for j in range(m):
+        if floor[i][j] in ('-', '|'):
+            dfs(i, j)
+            result += 1
+print(result)
