4b48c23ba76c98ad8e69194defa9b1efc0f2e19f
ankyobong <ankyobong@gmail.com>
2023-02-21 16:32:29 +0900

    물건 팔기

---
+++ silver4/물건 팔기.py
@@ -0,0 +1,23 @@
+# https://www.acmicpc.net/problem/1487
+sold = dict()
+n = int(input())
+for _ in range(n):
+    a, b = map(int, input().split())
+    if a not in sold:
+        sold[a] = [b]
+    else:
+        sold[a].append(b)
+result = 0
+top = 0
+for i in sorted(list(sold.keys())):
+    t = 0
+    for j in sold:
+        for z in sold[j]:
+            if i-z > 0:
+                t += i-z
+    del sold[i]
+    if top < t:
+        top = t
+        result = i
+
+print(result)
