51b60a0baf4c474f35c27ff93ebefd3f3ca456c7
ankyobong <ankyobong@gmail.com>
2023-03-03 22:52:36 +0900

    쓰래기 수거

--- silver4/쓰레기 수거.py
+++ silver4/쓰레기 수거.py
@@ -5,9 +5,13 @@ for _ in range(t):
     w_t = x_t = result = 0
     for i in range(n):
         x_i, w_i = map(int, input().split())
-        w_t += x_i
-        x_t += x_i
-        if w_t >= w:
-            result += x_i
-            w_t = x_t = 0
-    print(result)
+        result += x_i - x_t
+        x_t = x_i
+        w_t += w_i
+        if w_t == w and i != n -1:
+            result += 2 * x_i
+            w_t = 0
+        elif w_t > w:
+            result += 2 * x_i
+            w_t = w_i
+    print(result+x_i)
