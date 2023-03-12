9ba5195d07fa7faf6d99f03afb98aa9bd1798277
ankyobong <ankyobong@gmail.com>
2023-03-12 22:31:41 +0900

    사탕

--- silver4/사탕.py
+++ silver4/사탕.py
@@ -1,14 +1,14 @@
 # https://www.acmicpc.net/problem/1812
 n = int(input())
 canddy = []
-sum = 0
+sum_t = 0
 for i in range(n):
     a = int(input())
     canddy.append(a)
-    sum += a
-sum //= 2
+    sum_t += a
+sum_t //= 2
 for i in range(n):
     num = 0
     for j in range(1, n, 2):
         num += canddy[(i + j) % n]
-    print(sum - num)
+    print(sum_t - num)
