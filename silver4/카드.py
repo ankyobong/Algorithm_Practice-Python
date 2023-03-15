ff5ca3bc2a81953fd371974916486f56f39cd383
ankyobong <ankyobong@gmail.com>
2023-03-15 23:14:42 +0900

    카드
    deque 안쓴방법

--- silver4/카드.py
+++ silver4/카드.py
@@ -2,7 +2,8 @@
 n = int(input())
 result = [n]
 for i in reversed(range(1, n)):
-    result.append(result.pop(0))
-    result = [i] + result
-result = [result.pop(-1)] + result
-print(*result)
+    result.append(i)
+    for j in range(i):
+        result.append(result.pop(0))
+# result = [result.pop(-1)] + result
+print(*reversed(result))
