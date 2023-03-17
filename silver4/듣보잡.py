b5270fa563a65c0a8c103900b0ca85481a9e9fe2
ankyobong <ankyobong@gmail.com>
2023-03-17 23:41:05 +0900

    듣보잡

--- silver4/듣보잡.py
+++ silver4/듣보잡.py
@@ -2,11 +2,13 @@
 n, m = map(int, input().split())
 a = list()
 b = list()
+
 for _ in range(n):
     a.append(input())
 for _ in range(m):
     b.append(input())
 # result = sorted(set(a).intersection(b))
 result = sorted(set(a) & set(b))
+
 print(len(result))
 print(*result, sep='\n')
