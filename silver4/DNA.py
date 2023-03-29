2493a331fa7cc71f4f44ea796822588d5281f186
ankyobong <ankyobong@gmail.com>
2023-03-29 17:03:37 +0900

    DNA ing

---
+++ silver4/DNA.py
@@ -0,0 +1,10 @@
+# https://www.acmicpc.net/problem/1969
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+
+dna_list = list()
+
+for _ in range(n):
+    dna_list.append(sys.stdin.readline())
+    
