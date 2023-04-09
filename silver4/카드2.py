b942b105f481a7426cbccbbb9c2938d3b672c391
ankyobong <ankyobong@gmail.com>
2023-04-09 23:38:02 +0900

    카드2

---
+++ silver4/카드2.py
@@ -0,0 +1,11 @@
+# https://www.acmicpc.net/problem/2164
+from collections import deque
+
+N = int(input())
+deque = deque([i for i in range(1, N + 1)])
+
+while len(deque) > 1:
+    deque.popleft()
+    deque.append(deque.popleft())
+
+print(deque[0])
