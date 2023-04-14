128130c54eef166889cf6f523337906e145901e7
ankyobong <ankyobong@gmail.com>
2023-04-14 22:30:59 +0900

    설탕 배달

--- silver4/설탕 배달.py
+++ silver4/설탕 배달.py
@@ -1,14 +1,27 @@
 # https://www.acmicpc.net/problem/2839
-n = int(input())
-m = n//5
-result = -1
+# n = int(input())
+# m = n//5
+# result = -1
+#
+# for i in reversed(range(1, m+1)):
+#     if (n-(i*5)) % 3 == 0:
+#         result = i + ((n-(i*5))//3)
+#         break
+#
+# if result == -1 and n % 3 == 0:
+#     result = n//3
+#
+# print(result)
 
-for i in reversed(range(1, m+1)):
-    if (n-(i*5)) % 3 == 0:
-        result = i + ((n-(i*5))//3)
+
+x = int(input())
+a = 0
+while x >= 0:
+    if x % 5 == 0:
+        a += (x//5)
+        print(a)
         break
-
-if result == -1 and n % 3 == 0:
-    result = n//3
-
-print(result)
+    x = x - 3
+    a = a + 1
+else:
+    print(-1)
