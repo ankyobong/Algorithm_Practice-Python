07d20ad34a512a2bdea067acaa6640c99f66bc84
ankyobong <ankyobong@gmail.com>
2023-04-19 22:28:09 +0900

    균형잡힌 세상

--- silver4/균형잡힌 세상.py
+++ silver4/균형잡힌 세상.py
@@ -1,23 +1,35 @@
 # https://www.acmicpc.net/problem/4949
-import sys
+import sys, re
 
+a = ''
 while True:
-    n = sys.stdin.readline()
-    if n == '.':
-        print('yes')
+    a += sys.stdin.readline().rstrip()
+    if a == '.':
         break
+    elif a[-1] != '.':
+        continue
+    n = re.sub(r'([a-z,A-Z," "])', '', a)
     result = list()
     for i in n:
         if i in ('[', '('):
-            result.append([i])
+            result.append(i)
         elif i in (']', ')'):
-            if result[-1] == i:
-                result.pop()
-            elif i == ']' and result[-1] == '(':
-                break
-            elif i == ')' and result[-1] == '[':
+            if result:
+                if i == ']' and result[-1] == '[':
+                    result.pop()
+                elif i == ')' and result[-1] == '(':
+                    result.pop()
+                elif i == ']' and result[-1] == '(':
+                    result.append(i)
+                    break
+                elif i == ')' and result[-1] == '[':
+                    result.append(i)
+                    break
+            else:
+                result.append(i)
                 break
     if result:
         print('no')
     else:
         print('yes')
+    a = ''
