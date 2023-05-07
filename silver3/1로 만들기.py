452a4e0905bdc00316560a20c6b8641a7cdc7e26
ankyobong <ankyobong@gmail.com>
2023-05-07 20:41:33 +0900

    1로 만들기

---
+++ silver3/1로 만들기.py
@@ -0,0 +1,11 @@
+# https://www.acmicpc.net/problem/1463
+n = int(input())
+x = [0]*(n+1)
+for i in range(2, n+1):
+    x[i] = x[i-1] + 1
+    if i % 2 == 0:
+        x[i] = min(x[i], x[i//2]+1)
+    if i % 3 == 0:
+        x[i] = min(x[i], x[i//3]+1)
+
+print(x[n])
