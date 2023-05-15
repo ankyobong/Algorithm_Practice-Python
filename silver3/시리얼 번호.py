01d2e0f9e89404ef6e01da767c66cf67f3408020
ankyobong <ankyobong@gmail.com>
2023-05-15 22:27:13 +0900

    시리얼 번호

--- silver3/시리얼 번호.py
+++ silver3/시리얼 번호.py
@@ -6,9 +6,10 @@ d = dict()
 for _ in range(n):
     inp = input()
     if len(inp) in d:
-        d[len(inp)][inp] = sum([int(i) for i in re.sub(f'[^1-9]', '', '145C')])
+        d[len(inp)][inp] = sum([int(i) for i in re.sub(f'[^1-9]', '', inp)])
     else:
-        d[len(inp)] = {inp: sum([int(i) for i in re.sub(f'[^1-9]', '', '145C')])}
+        d[len(inp)] = {inp: sum([int(i) for i in re.sub(f'[^1-9]', '', inp)])}
 
 for i in sorted(d.keys()):
-    print(d[i])
+    for j in sorted(sorted(d[i].items()), key=lambda x: x[1]):
+        print(j[0])
