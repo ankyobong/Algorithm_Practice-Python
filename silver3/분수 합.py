a5ef84fc0d24ac31e80f55443e56bad9e19c161d
ankyobong <ankyobong@gmail.com>
2023-05-19 00:02:25 +0900

    분수 합

--- silver3/분수 합.py
+++ silver3/분수 합.py
@@ -1,14 +1,16 @@
 # https://www.acmicpc.net/problem/1735
-from math import gcd
+import fractions
 a, b = map(int, input().split())
 c, d = map(int, input().split())
 
 x, y = 0, 0
 if b == d:
     x = a + c
-    y = b + d
+    y = b
 else:
     x = (a*d) + (c*b)
     y = b * d
 
-print(int(x/gcd(x, y)), int(y/gcd(x, y)))
+Irr = fractions.Fraction(x, y)
+
+print(Irr.numerator, Irr.denominator)
