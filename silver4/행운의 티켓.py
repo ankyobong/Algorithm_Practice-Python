5ed330c1b3bfeaab5352eb202c81ae1e8f9c988f
ankyobong <ankyobong@gmail.com>
2023-06-05 22:01:12 +0900

    행운의 티켓

--- silver4/행운의 티켓.py
+++ silver4/행운의 티켓.py
@@ -1,10 +1,12 @@
 # https://www.acmicpc.net/problem/1639
 num = list(map(int, list(input())))
 result = 0
+
 for i in range(len(num)):
     for j in range(i+1, len(num), 2):
         t = num[i:j+1]
         if sum(t[:len(t)//2]) == sum(t[len(t)//2:]):
             if result < len(t):
                 result = len(t)
+
 print(result)
