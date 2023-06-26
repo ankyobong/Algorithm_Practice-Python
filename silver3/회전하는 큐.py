04db6568c57dfd31259853f83b9231c6ab5c3dc4
ankyobong <ankyobong@gmail.com>
2023-06-26 23:27:56 +0900

    큐

--- silver3/회전하는 큐.py
+++ silver3/회전하는 큐.py
@@ -5,7 +5,6 @@ n, m = map(int, input().split())
 position = list(map(int, input().split()))
 dq = deque([i for i in range(1, n+1)])
 count = 0
-
 for i in position:
     while True:
         if dq[0] == i:
